74a69d6c3f4ed57fefc9f9f5cb3339d46bf4f512
Lindolfo Moizinho <lindolfomoizinho@gmail.com>
2024-06-27 09:09:04 -0300

    Initial Commit

---
+++ src/main/java/br/com/libdolf/bookrecommendation/BookRecommendationApplication.java
@@ -0,0 +1,13 @@
+package br.com.libdolf.bookrecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRecommendationApplication.class, args);
+	}
+
+}
